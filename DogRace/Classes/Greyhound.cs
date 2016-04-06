416532cbdc03bc02a77e966d747ba23a7708c742
kennyboi89 <ken_td@hotmail.com>
2016-04-06 10:06:50 +0200

    DogTrack solution

---
+++ DogRace/Classes/Greyhound.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DogRace.Classes
+{
+    public class Greyhound
+    {
+        public int StartingPos;
+        public int RaceTrackLenght;
+        public PictureBox MyPictureBox = null;
+        public int Location = 0;
+        public Random Randomizer;
+
+        public bool Run()
+        {
+            
+            return true;
+        }
+        
+        public void TakeStartingPos()
+        {
+            Location = 0;
+            StartingPos = 0;
+        }
+    }
+}
